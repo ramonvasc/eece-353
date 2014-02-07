8d2ed22815e27356400c3490d9c5aab4bdd3f1cf
ramonvasc <ramonvasc@gmail.com>
2014-02-06 21:18:45 -0800

    State Machine
    primeira try

--- card7seg.vhd
+++ card7seg.vhd
@@ -10,10 +10,35 @@ END;
 
 
 ARCHITECTURE behavioral OF card7seg IS
+signal inv : std_logic_vector(6 downto 0);
 BEGIN
 
    -- your code goes here.  Hint: this is a simple combinational block
    -- like you did in Lab 1.  If you find this difficult, you are on the
    -- wrong track.
 
+		process(card)
+		begin
+		case card (3 downto 0) is
+			when "0000" => inv <= "1111111"; --no cards
+			when "0001" => inv <= "1111001"; --1
+			when "0010" => inv <= "0100100"; --2
+			when "0011" => inv <= "0110000"; --3           
+			when "0100" => inv <= "0011001"; --4
+			when "0101" => inv <= "0010010"; --5
+			when "0110" => inv <= "0000010"; --6
+			when "0111" => inv <= "1111000"; --7
+			when "1000" => inv <= "0000000"; --8
+			when "1001" => inv <= "0010000"; --9
+			when "1010" => inv <= "1000000"; --10
+			when "1011" => inv <= "1110001"; --J
+			when "1100" => inv <= "0011000"; --Q
+			when others => inv <= "0001001"; --H
+		end case;
+		end process;
+		
+		process(inv)
+		begin
+		seg7 <= inv;
+		end process;
 END;
--- statemachine.vhd
+++ statemachine.vhd
@@ -24,8 +24,66 @@ END statemachine;
 
 
 ARCHITECTURE behavioural OF statemachine IS
-
+signal present_state : STD_LOGIC_VECTOR (3 downto 0);
+begin
 -- Your code goes here.  This is the most challenge of all the files
 -- to write.
+	process(resetb,slow_clock)
+	--variable present_state : std_logic_vector (3 downto 0);
+	begin
+	if(resetb = '0') then
+		present_state <= "0000";	
+	elsif (slow_clock'event and slow_clock = '1') then
+		case (present_state) is
+			when "0000" => load_pcard1 <= '1'; --player gets first card
+			present_state <= "0001";
+			when "0001" => load_dcard1 <= '1'; --dealer gets first card
+			present_state <= "0010";
+			when "0010" => load_pcard2 <= '1'; --player gets second card
+			present_state <= "0011"; 
+			when "0011" => load_dcard2 <= '1'; --dealer gets second card
+					if((unsigned(dscore) or unsigned(pscore)) >= 8) then
+						present_state <= "0100";
+					elsif(unsigned(pscore) >=0 and unsigned(pscore) <=5) then
+						present_state <= "0101";
+					elsif(unsigned(pscore) = 6 or unsigned(pscore) =7) then
+						present_state <= "0110";
+					end if;
+			when "0100" => --game over
+					if (unsigned(dscore) > unsigned(pscore)) then
+						LEDG(1) <= '1'; --dealer wins
+					elsif(unsigned(dscore) < unsigned(pscore)) then
+						LEDG(0) <= '1'; --player wins
+					elsif (unsigned(dscore) = unsigned(pscore)) then
+						LEDG(0) <= '1';
+						LEDG(1) <= '1'; --tie
+					end if;
+			when "0101" => load_pcard3 <= '1'; --player has between 0 and 5 points
+					if(unsigned(dscore) = 7) then
+						present_state <= "0100";
+					elsif(unsigned(dscore) = 6 and (pcard3 = "0110" or pcard3 = "0111")) then --pcard3 is 6 or 7
+						present_state <= "0111";
+					elsif(unsigned(dscore) = 5 and (pcard3 = "0100" or pcard3 = "0101" or pcard3 = "0110" or pcard3 = "0111")) then --pcard3 is 4,5,6 or 7
+						present_state <= "0111";
+					elsif(unsigned(dscore) = 4 and (pcard3 = "0010" or pcard3 = "0011" or pcard3 = "0100" or pcard3 = "0101" or pcard3 = "0110" or pcard3 = "0111")) then --pcard3 is 2,3,4,5,6 or 7
+						present_state <= "0111";
+					elsif(unsigned(dscore) = 3 and pcard3 /= "1000") then --pcard3 is anything but 8
+						present_state <= "0111";
+					elsif((unsigned(dscore) = 0) or (unsigned(dscore) = 1) or (unsigned(dscore) = 2)) then
+						present_state <= "0111";
+					end if;
+			when "0110" => --player has 6 or 7 points
+					if (unsigned(dscore) >= 0 and unsigned(dscore) <=5) then
+						present_state <= "0111";
+					else
+						present_state <= "0100";
+					end if;
+			when "0111" => load_dcard3 <= '1'; --dealer gets the third card
+			present_state <= "0100";
+			when others => present_state <="0000";
+			end case;
+	end if;
+	end process;
+		
 
 END;
